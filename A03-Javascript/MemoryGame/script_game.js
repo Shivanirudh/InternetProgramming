11774c68120655d4269936b059b5db7da8e8082a
Shivanirudh <shivanirudh2001@gmail.com>
2021-03-11 14:26:47 +0530

    Game Javascript

--- A03-Javascript/MemoryGame/script_game.js
+++ A03-Javascript/MemoryGame/script_game.js
@@ -1,90 +1,26 @@
-var imgArray = new Array();
-imgArray[0] = new Image(); 
-imgArray[0].src = 'Pics/aang.jpg';
-imgArray[1] = new Image();
-imgArray[1].src = 'Pics/aang.jpg';
-
-imgArray[2] = new Image(); 
-imgArray[2].src = 'Pics/appa.jpg';
-imgArray[3] = new Image();
-imgArray[3].src = 'Pics/appa.jpg';
-
-imgArray[4] = new Image(); 
-imgArray[4].src = 'Pics/azula.jpg';
-imgArray[5] = new Image();
-imgArray[5].src = 'Pics/azula.jpg';
-
-imgArray[6] = new Image();
-imgArray[6].src = 'Pics/bumi.jpg';
-imgArray[7] = new Image();
-imgArray[7].src = 'Pics/bumi.jpg';
-
-imgArray[8] = new Image(); 
-imgArray[8].src = 'Pics/iroh.jpg';
-imgArray[9] = new Image();
-imgArray[9].src = 'Pics/iroh.jpg';
-
-imgArray[10] = new Image(); 
-imgArray[10].src = 'Pics/katara.png';
-imgArray[11] = new Image();
-imgArray[11].src = 'Pics/katara.png';
-
-imgArray[12] = new Image(); 
-imgArray[12].src = 'Pics/momo.png';
-imgArray[13] = new Image();
-imgArray[13].src = 'Pics/momo.png';
-
-imgArray[14] = new Image(); 
-imgArray[14].src = 'Pics/Sokka.png';
-imgArray[15] = new Image();
-imgArray[15].src = 'Pics/Sokka.png';
-
-imgArray[16] = new Image(); 
-imgArray[16].src = 'Pics/toph.jpg';
-imgArray[17] = new Image();
-imgArray[17].src = 'Pics/toph.jpg';
-
-imgArray[18] = new Image(); 
-imgArray[18].src = 'Pics/zuko.jpg';
-imgArray[19] = new Image();
-imgArray[19].src = 'Pics/zuko.jpg';
+const cells = document.querySelectorAll(".grid-item");
 
 var score;
 let openedCards = [];
 let matchedCards =  [];
 var flip = false;
+var pause = false;
+var countDown = 0;
 
-function shuffleArray(array) {  
-    return array.sort( ()=>Math.random()-0.5 ); 
-}  
-
-var cells = document.getElementsByClassName("grid-item");
-var cellsArray = [...cells];
-var imgArray1 = shuffleArray(imgArray);
+var level = document.getElementById("level").value;
+if (level == "Normal"){
+    countDown = 120;
+}
+else if (level == "Easy") {
+    countDown = 90;
+} else {
+    countDown = 60;
+}
 
 document.getElementById("start").addEventListener("click", function(){
     document.getElementById("game").style.display = "block";
-    var level = document.getElementById("level").value;
-    if (level == "Normal"){
-        var countDown = 120;
-    }
-    else if (level == "Easy") {
-        var countDown = 90;
-    } else {
-        var countDown = 60;
-    }
+    
     var i;
-    for(i = 0; i < 20; i++){
-        var id = "item" + (i+1).toString();
-        var idb = id + "-b";
-        document.getElementById(id).src='Pics/logo.jpg';
-        document.getElementById(id).style.height="100px";
-        document.getElementById(id).style.width="100px";
-        document.getElementById(idb).src=imgArray1[i].src;
-        document.getElementById(idb).style.height="100px";
-        document.getElementById(idb).style.width="100px";
-        document.getElementById(idb).style.display = "none";
-    }
 
     score = 0;
     document.getElementById("score").innerText = "Score:" + score.toString();
@@ -106,6 +42,70 @@ document.getElementById("start").addEventListener("click", function(){
     location.href = "#game";
 });
 
+function showCard(){
+
+    if(pause){
+        return;
+    }
+
+    if(this === firstCard){
+        return;
+    }
+
+    this.classList.add("flip");
+
+    if(!flip){
+        flip = true;
+        openedCards[0] = this;
+        return;
+    }
+    
+    openedCards[1] = this;
+    
+    if(openedCards[0].value === openedCards[1].value){
+        pause = true;   
+        setTimeout(disableCards, 450);
+        updateScore();
+    }
+    else{
+        hideCards();
+    }
+}
+
+function disableCards(){
+    //Disables the chosen cards
+
+    openedCards[0].removeEventListener("click", showCard);
+    openedCards[1].removeEventListener("click", showCard);
+
+    openedCards[0].style.setProperty("visibility", "hidden");
+    openedCards[1].style.setProperty("visibility", "hidden");
+
+    resetGame();
+}
+
+function hideCards(){
+
+    lockBoard = true;
+
+    setTimeout(() => {
+        openedCards[0].classList.remove("flip");
+        openedCards[1].classList.remove("flip");
+        resetGame();
+    }, 1000);
+}
+
+function resetGame(){
+
+    [flip, pause] = [false, false];
+    openedCards = [];
+}
+
+
+function shuffleArray(array) {  
+    return array.sort( ()=>Math.random()-0.5 ); 
+}  
+/*
 function setImg(){
     
     if(this === openedCards[0]){
@@ -200,7 +200,7 @@ function enable() {
     })
 }
 
-/*
+
 function flashCards() {
     for(i=0; i<cardElements.length; i++) {
         cells[i].children[0].classList.add("show-img")
