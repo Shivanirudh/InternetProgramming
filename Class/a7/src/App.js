1ac8bc947b8b89e98ba9de55f9def0a1820a03e0
Shivanirudh <shivanirudh2001@gmail.com>
2021-04-05 10:31:26 +0530

    react

---
+++ Class/a7/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">React assignment</h1>
+      <hr/>
+      <br/>
+      <h3 className="content">Hello world</h3>
+    </div>
+
+  );
+}
+
+export default App;
